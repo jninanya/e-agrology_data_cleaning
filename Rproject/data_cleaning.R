a0dec0b46e6c127bdc8980278f267d57483db84c
Johan Ninanya (noni) <ninanyat.jl@gmail.com>
2024-02-26 08:10:27 -0500

    upload files

--- Rproject/data_cleaning.R
+++ Rproject/data_cleaning.R
@@ -4,25 +4,25 @@ rm(list=ls())
 # libraries
 library(openxlsx)
 library(dplyr)
+library(stringi)
 
-# read data
+# read data from github
+github_url_xlsx <- "https://github.com/jninanya/e-agrology_data_cleaning/raw/main/raw_data/Bitacora%20agronomica-Peru_MEAL_04%20de%20enero%202024.xlsx"
 
-xlsx = "Bitacora agronomica-Peru_MEAL_04 de enero 2024.xlsx"
-
-d1 <- read.xlsx(xlsx, sheet = 1, startRow = 2)
-d2 <- read.xlsx(xlsx, sheet = 2)
-d3 <- read.xlsx(xlsx, sheet = 3)
-d4 <- read.xlsx(xlsx, sheet = 4)
-d5 <- read.xlsx(xlsx, sheet = 5)
-d6 <- read.xlsx(xlsx, sheet = 6)
-d7 <- read.xlsx(xlsx, sheet = 7)
+d1 <- read.xlsx(github_url_xlsx, sheet = 1, startRow = 2)
+d2 <- read.xlsx(github_url_xlsx, sheet = 2)
+d3 <- read.xlsx(github_url_xlsx, sheet = 3)
+d4 <- read.xlsx(github_url_xlsx, sheet = 4)
+d5 <- read.xlsx(github_url_xlsx, sheet = 5)
+d6 <- read.xlsx(github_url_xlsx, sheet = 6)
+d7 <- read.xlsx(github_url_xlsx, sheet = 7)
 
 ################################################################################
 ### 01 - Farmers-crop-site 
 ################################################################################
 
-
-
+d1$name <- tolower(d1$name)
+d1$name <- stri_trans_general(d1$name, id="Latin-ASCII")
 
 
 
